7a1c5e85630107bb9e9d66b999f52f24456681f7
CrusaderSquirrel <jensberckmoes@hotmail.com>
2025-03-31 21:39:20 +0200

    Added constraints to name and added the tests for it

--- src/main/java/be/jensberckmoes/personal_finance_tracker/model/Category.java
+++ src/main/java/be/jensberckmoes/personal_finance_tracker/model/Category.java
@@ -1,6 +1,8 @@
 package be.jensberckmoes.personal_finance_tracker.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,7 +20,9 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
+    @NotBlank(message = "Category name cannot be blank")
+    @Size(max = 50, message = "Category name cannot exceed 50 characters")
+    @Column(name = "name", nullable = false, unique = true, length = 50)
     private String name;
 
     @Column(name = "description")
--- src/test/java/be/jensberckmoes/personal_finance_tracker/integration/repository/CategoryRepositoryTest.java
+++ src/test/java/be/jensberckmoes/personal_finance_tracker/integration/repository/CategoryRepositoryTest.java
@@ -3,10 +3,13 @@ package be.jensberckmoes.personal_finance_tracker.integration.repository;
 import be.jensberckmoes.personal_finance_tracker.model.Category;
 import be.jensberckmoes.personal_finance_tracker.repository.CategoryRepository;
 import jakarta.persistence.EntityManager;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.Optional;
@@ -14,6 +17,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class CategoryRepositoryTest {
     @Autowired
     private CategoryRepository categoryRepository;
@@ -55,5 +59,46 @@ public class CategoryRepositoryTest {
 
         assertTrue(possibleFoundCategory.isPresent());
         assertTrue(possibleNonFoundCategory.isEmpty());
+    }
+
+    @Test
+    public void givenCategoryWithBlankName_whenSave_thenThrowsException() {
+        final Category category = Category.builder()
+                .name(" ")
+                .description("Test")
+                .build();
+        assertThrows(ConstraintViolationException.class, () -> categoryRepository.save(category));
+    }
+
+    @Test
+    public void givenCategoryWithLongName_whenSave_thenThrowsException() {
+        final Category category = Category.builder()
+                .name("a".repeat(51))
+                .description("Test")
+                .build();
+        assertThrows(ConstraintViolationException.class, () -> categoryRepository.save(category));
+    }
+
+    @Test
+    public void givenCategoryWithNullName_whenSave_thenThrowsException() {
+        final Category category = Category.builder()
+                .name(null)
+                .description("Test")
+                .build();
+        assertThrows(ConstraintViolationException.class, () -> categoryRepository.save(category));
+    }
+
+    @Test
+    public void givenDuplicateCategoryName_whenSave_thenThrowsException() {
+        final Category category = Category.builder()
+                .name("Duplicate")
+                .description("Test")
+                .build();
+        categoryRepository.save(category);
+        final Category duplicateCategory = Category.builder()
+                .name("Duplicate")
+                .description("Test2")
+                .build();
+        assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(duplicateCategory));
     }
 }
